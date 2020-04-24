f0e4fe1feccdffa1d0e1aa3153f1b574c81d7539
Joey Ferenchak <sjferenchak@gmail.com>
2020-04-24 13:39:49 -0400

    Added lab5 files

---
+++ assigments/worldSeriesChampions.java
@@ -0,0 +1,12 @@
+/*
+    Write a program that lets the user enter the name of a team and then displays
+    the number of times that team has won the World Series.
+    The WorldSeriesWinners.txt file contains a chronological list of the world series winning
+    the World Series in the time period from 1903 to 2018.
+**/
+
+public class worldSeriesChampions{
+    public static void main(String[] args){
+
+    }
+}
