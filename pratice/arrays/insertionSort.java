66af567a09a807aa7b663cddb6826f229458ff6d
Joey Ferenchak <sjferenchak@gmail.com>
2020-04-24 11:53:51 -0400

    Add insertion sort file, rename sort.java to printArray.java

---
+++ pratice/arrays/insertionSort.java
@@ -0,0 +1,28 @@
+public class insertionSort {
+    public static void main(String[] args){
+        //int myArray[] = {1, 3, 2, 10, 7, 6, 9, 5, 8, 4};
+        int myArray[] = {1,7,9,8};
+
+        //print out the unorganized array
+        for(int element : myArray)
+            System.out.print(element + " ");
+        System.out.println();
+
+        //insertion sort
+        for(int i = 1; i < myArray.length; i++) {
+            int key = myArray[i];
+            int j = i - 1;
+
+            while(j >= 0 && myArray[j] > key){
+                myArray[j + 1] = myArray[j];
+                j  = j - 1;
+            }
+            myArray[j + 1] = key;
+        }
+
+        //print out sorted array
+        for(int element: myArray)
+            System.out.print(element + " ");
+        System.out.println();
+    }
+}
---
+++ pratice/arrays/printArray.java
@@ -0,0 +1,23 @@
+public class printArray {
+  public static void main(String[] args){
+    //array that stores doubles
+    double[] myList = {1, 2, 3, 4, 5};
+
+    int firstVar = 0;
+
+    //counting the array backwards
+    for (int i = myList.length - 1; i >= 0; i--) {
+      if(i == firstVar){
+        System.out.println(myList[i] + "\n");
+      }
+      else {
+        System.out.println(myList[i]);
+      }
+    }
+
+    //for each loop
+    for(double element : myList) {
+      System.out.println(element);
+    }
+  }
+}
